1e3212b22f064136a3ca31610e41a1713021e1e9
Ingrid <ingridstanic@gmail.com>
2026-02-11 18:41:11 +0100

    implemented frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,24 +1,20 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { addGame, getGames } from "./services/gameService";
+import "./style.css";
+import { createHtml } from "./utils/htmlUtils";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const games = await getGames();
+createHtml(games);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+document.getElementById("gameForm")?.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const userInput = (document.getElementById("gameText") as HTMLInputElement)
+    .value;
+
+  await addGame(userInput);
+
+  (document.getElementById("gameText") as HTMLInputElement).value = "";
+
+  const newGames = await getGames();
+  createHtml(newGames);
+});
